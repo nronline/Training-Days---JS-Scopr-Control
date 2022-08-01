91a55172fc326d79df3b772bbcf5631de7fcf7e7
Nick <nronlinewm@gmail.com>
2022-08-01 21:33:44 +0100

    Create main.js
    Initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+// define name variable
+const name = 'Nala';
+
+
+const getRandEvent = () => {
+    
+    const random = Math.floor(Math.random() * 3);
+
+    if (random === 0) {
+        return 'Marathon';
+    } else if (random === 1) {
+        return 'Triathlon';
+    } else if (random === 2) {
+        return 'Pentathlon';
+    }
+};
+
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+    let days;
+
+    if (event === 'Marathon') {
+        days = 50;
+    } else if (event === 'Triathlon') {
+        days = 100;
+    } else if (event === 'Pentathlon') {
+        days = 200;
+    }
+
+    return days;
+};
+
+// Arrow function passing name and event params to log event to console
+const logEvent = (name, event) => {
+    console.log(`${name}'s event is: ${event}`);
+};
+
+// Arrow function passing name and event params to log duration to console
+const logTime = (name, days) => {
+    console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+
+
+logEvent(name, event);
+logTime(name, days);
+
+// generate second event and participant
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+logEvent(name2, event2);
+logTime(name2, days2);
